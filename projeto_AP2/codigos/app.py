1766702f5d3ff07a12a547d6b6f94a2e0d2b454f
buenobarretolais@gmail.com <buenobarretolais@gmail.com>
2025-06-03 21:07:09 -0300

    teste

--- projeto_AP2/codigos/app.py
+++ projeto_AP2/codigos/app.py
@@ -26,7 +26,7 @@ st.title('Dashboard de Análise de Dados de Produtos AliExpress')
 
 # --- Carregamento dos Dados (feito uma vez no início) ---
 try:
-    df = pd.read_csv('../bases_tratadas/dados_tratados.csv', sep=';')
+    df = pd.read_csv('bases_tratadas/dados_tratados.csv', sep=';')
 except FileNotFoundError:
     st.error("Erro: O arquivo 'dados_tratados.csv' não foi encontrado. Por favor, verifique o caminho.")
     st.stop()
@@ -243,3 +243,4 @@ elif st.session_state.selected_page == "Análises Multivariadas":
         st.info('Por favor, escolha **mais uma coluna** para visualizar os gráficos de relação.')
     else:
         st.info('Selecione duas colunas para visualizar os gráficos de dispersão e caixa.')
+        
